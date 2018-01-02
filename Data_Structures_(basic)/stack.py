78d9f19335e9af18db2270a07096a96f451ae584
jayjoshi64 <jayjoshi7064@gmail.com>
2018-01-02 18:20:40 +0530

    few bug fix and updated Stack,Queue,Circular Queue init method for dynamic stack isempty return boolean instead of printing naming convention topfromStack to top pop returns the object
    bug in queue remove function
    peek in queue checks if the list is empty or not
    
    circular queue remove bug in setting new head

--- Data_Structures_(basic)/stack.py
+++ Data_Structures_(basic)/stack.py
@@ -1,19 +1,21 @@
 class Stack:
-	max = 5
-	top = -1
-	array = [None] * max
+
+	def __init__(self,size):
+		self.size = size
+		self.top = -1
+		self.array = [None] * self.size
 
 	def isEmpty(self):
 		if(self.top == -1):
-			print(True)
+			return True
 		else:
-			print(False)
+			return False
 
-	def topOfStack(self):
-		print('First data in stack is : ' + str(self.array[self.top]))
+	def top(self):
+		return self.array[self.top]
 
 	def push(self, data):
-		if (self.top == 4):
+		if not (self.top < self.size-1):
 			print('Stack is full')
 			return
 		self.top+=1
@@ -23,13 +25,17 @@ class Stack:
 	def pop(self):
 		if (self.top == -1):
 			print('Stack is empty')
+			return None
 		else:
 			print(str(self.array[self.top]) + ' was deleted')
+			obj = self.array[self.top]
 			self.array[self.top] = None	
 			self.top-=1			
+			return obj
 
 	def display(self):
 		if (self.top == -1):
 			print('Stack is empty')
 		else:
 			print('Stack is : ' + str(self.array) )
+
