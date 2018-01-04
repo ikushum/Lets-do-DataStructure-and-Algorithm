d7a5da5620cbf8d63e078131bda201beb6519089
Ishan Subedi <ikushum@gmail.com>
2018-01-04 23:56:25 +0545

    bst add inorder and refactor

--- Data_Structures_(basic)/binary_tree.py
+++ Data_Structures_(basic)/binary_tree.py
@@ -7,18 +7,21 @@ class Node:
 	def insert(self, data):
 		if(self.data == data):
 			print(str(data) + ' already exists')
+			return False
 		elif(self.data > data):
 			if(self.left_child):
-				self.left_child.insert(data)
+				return self.left_child.insert(data)
 			else: 
 				self.left_child = Node(data)
 				print(str(data)+' was inserted')
+				return True
 		else:
 			if(self.right_child):
-				self.right_child.insert(data)
+				return self.right_child.insert(data)
 			else: 
 				self.right_child = Node(data)
 				print(str(data)+' was inserted')
+				return True
 
 	def find(self, data):
 		if(data == self.data):
@@ -34,21 +37,38 @@ class Node:
 		if(data == self.data):
 			nodeToDelete = self
 			if(nodeToDelete.left_child == nodeToDelete.right_child == None):
-				nodeToDelete = None
+				if(parent.left_child == nodeToDelete):
+					parent.left_child=None
+				else:
+					parent.right_child=None
 			elif(nodeToDelete.left_child == None):
-				nodeToDelete = nodeToDelete.right_child
+				if(parent.left_child == nodeToDelete):
+					parent.left_child=nodeToDelete.right_child
+				else:
+					parent.right_child=nodeToDelete.right_child
 			elif(nodeToDelete.right_child == None):
-				nodeToDelete = nodeToDelete.left_child
+				if(parent.left_child == nodeToDelete	):
+					parent.left_child=nodeToDelete.left_child
+				else:
+					parent.right_child=nodeToDelete.left_child
 			else:
 				min_node = nodeToDelete.findMinNode()
 				nodeToDelete.data = min_node.data
 				self = nodeToDelete.right_child
 				self.delete(min_node.data, parent=nodeToDelete)
-			return data
+			return(data)
 		elif(data < self.data):
-			self.left_child.delete(data,parent=self) if self.left_child else print(str(data) + ' not found')
+			return(self.left_child.delete(data,parent=self)) if self.left_child else print(str(data) + ' not found')
 		elif(data > self.data) :
-			self.right_child.delete(data,parent=self) if self.right_child else print(str(data) + ' not found')
+			return(self.right_child.delete(data,parent=self)) if self.right_child else print(str(data) + ' not found')
+
+	def inorder(self):
+		if self:
+			if self.left_child:
+				self.left_child.inorder()
+			print (self.data)
+			if self.right_child:
+				self.right_child.inorder()
 
 	def findMinNode(self):
 		min_node = self.right_child
@@ -85,17 +105,24 @@ class Tree:
 		else:
 			print('Tree is empty')
 
-bst = Tree()
+	def inorder(self):
+		if(self.parent):
+			self.parent.inorder()
+		else:
+			print('Tree is empty')
+
 choice = 0
-while(choice != 4):
-	print("\n1) Press 1 to insert\n2) Press 2 to search\n3) Press 3 to Delete\n4) Press 4 to Quit")
+while(choice != 5):
+	print("\n1) Press 1 to insert\n2) Press 2 to search\n3) Press 3 to Delete\n4) Press 4 to display inorder\n5) Press 5 to Quit")
 	choice = int(input("Enter your choice: "))
 	if(choice==1):
-		data = input("\nEnter number to insert: ")
+		data = int(input("\nEnter number to insert: "))
 		bst.insert(data)
 	elif(choice==2):
-		data = input("\nEnter number to find: ")
+		data = int(input("\nEnter number to find: "))
 		bst.find(data)
 	elif(choice==3):
-		data = input("\nEnter number to delete: ")
+		data = int(input("\nEnter number to delete: "))
 		bst.delete(data)
+	elif(choice==4):
+		bst.inorder()		
