7f123d357c07c0a633308fcbaf9403af4e41c8d0
Ishan Subedi <ikushum@gmail.com>
2018-01-05 15:53:26 +0545

    bst OOP convention refactor

--- Data_Structures_(basic)/binary_search_tree.py
+++ Data_Structures_(basic)/binary_search_tree.py
@@ -4,113 +4,96 @@ class Node:
 		self.left_child=None
 		self.right_child=None
 
+class Tree:
+	def __init__(self, data):
+		self.parent = Node(data)
+
 	def insert(self, data):
-		if(self.data == data):
-			print(str(data) + ' already exists')
-			return False
-		elif(self.data > data):
-			if(self.left_child):
-				return self.left_child.insert(data)
-			else: 
-				self.left_child = Node(data)
-				print(str(data)+' was inserted')
-				return True
+		if not(self.parent.data): 
+			self.parent.data = data
+			print(str(data) + ' was inserted')
 		else:
-			if(self.right_child):
-				return self.right_child.insert(data)
-			else: 
-				self.right_child = Node(data)
-				print(str(data)+' was inserted')
-				return True
+			if(self.parent.data == data):
+				print(str(data) + ' already exists')
+				return False
+			elif(self.parent.data > data):
+				if(self.parent.left_child):
+					return self.parent.left_child.insert(data)
+				else: 
+					self.parent.left_child = Tree(data)
+					print(str(data)+' was inserted')
+					return True
+			else:
+				if(self.parent.right_child):
+					return self.parent.right_child.insert(data)
+				else: 
+					self.parent.right_child = Tree(data)
+					print(str(data)+' was inserted')
+					return True			
 
 	def find(self, data):
-		if(data == self.data):
-			print(str(data) + ' is present')
-			return True
-		elif(data < self.data):
-			self.left_child.find(data) if self.left_child else print(str(data) + ' not found')
-		elif(data > self.data) :
-			self.right_child.find(data) if self.right_child else print(str(data) + ' not found')
-		return False
+		if(self.parent.data):
+			if(data == self.parent.data):
+				print(str(data) + ' is present')
+				return True
+			elif(data < self.parent.data):
+				self.parent.left_child.find(data) if self.parent.left_child else print(str(data) + ' not found')
+			elif(data > self.parent.data) :
+				self.parent.right_child.find(data) if self.parent.right_child else print(str(data) + ' not found')
+			return False
+		else:
+			print('Tree is empty') 
+			return False
 
-	def delete(self, data, parent=None):
-		if(data == self.data):
-			nodeToDelete = self
-			if(nodeToDelete.left_child == nodeToDelete.right_child == None):
-				if(parent.left_child == nodeToDelete):
-					parent.left_child=None
+	def delete(self,data,delNodeParent=None):
+		if(self.parent.data):
+			if(data == self.parent.data):
+				delNode = self.parent
+				if(delNode.left_child== delNode.right_child == None):
+					if(delNodeParent.left_child == self):
+						delNodeParent.left_child=None
+					else:
+						delNodeParent.right_child=None
+				elif(delNode.left_child == None):
+					if(delNodeParent.left_child == self):
+						delNodeParent.left_child=delNode.right_child
+					else:
+						delNodeParent.right_child=delNode.right_child
+				elif(delNode.right_child == None):
+					if(delNodeParent.left_child == self):
+						delNodeParent.left_child=delNode.left_child
+					else:
+						delNodeParent.right_child=delNode.left_child
 				else:
-					parent.right_child=None
-			elif(nodeToDelete.left_child == None):
-				if(parent.left_child == nodeToDelete):
-					parent.left_child=nodeToDelete.right_child
-				else:
-					parent.right_child=nodeToDelete.right_child
-			elif(nodeToDelete.right_child == None):
-				if(parent.left_child == nodeToDelete	):
-					parent.left_child=nodeToDelete.left_child
-				else:
-					parent.right_child=nodeToDelete.left_child
-			else:
-				min_node = nodeToDelete.findMinNode()
-				nodeToDelete.data = min_node.data
-				self = nodeToDelete.right_child
-				self.delete(min_node.data, parent=nodeToDelete)
-			return(data)
-		elif(data < self.data):
-			return(self.left_child.delete(data,parent=self)) if self.left_child else print(str(data) + ' not found')
-		elif(data > self.data) :
-			return(self.right_child.delete(data,parent=self)) if self.right_child else print(str(data) + ' not found')
+					min_node = self.findMinNode()
+					delNode.data = min_node.parent.data
+					self = delNode.right_child
+					self.delete(min_node.parent.data, delNodeParent=self)
+				return True
+			elif(data < self.parent.data):
+				return self.parent.left_child.delete(data,delNodeParent=self.parent) if self.parent.left_child else False
+			elif(data > self.parent.data) :
+				return self.parent.right_child.delete(data,delNodeParent=self.parent) if self.parent.right_child else False
+		else:
+			return False
 
 	def inorder(self):
-		if self:
-			if self.left_child:
-				self.left_child.inorder()
-			print (self.data)
-			if self.right_child:
-				self.right_child.inorder()
+		if(self.parent.data):
+			if self.parent.left_child:
+				self.parent.left_child.inorder()
+			print (self.parent.data)
+			if self.parent.right_child:
+				self.parent.right_child.inorder()
+		else:
+			print('Tree is empty')
 
 	def findMinNode(self):
-		min_node = self.right_child
-		while(min_node.left_child != None):
-			min_node = min_node.left_child
-		return min_node
+		min_node = self.parent.right_child
+		while(min_node.parent.left_child != None):
+			min_node = min_node.parent.left_child
+		return min_node		
 
-class Tree:
-	def __init__(self):
-		self.parent = None
-
-	def insert(self, data):
-		if(self.parent): 
-			self.parent.insert(data)
-		else:
-			self.parent = Node(data)
-			print(str(data) + ' was inserted')
-
-	def find(self, data):
-		if(self.parent):
-			search_result = self.parent.find(data)
-			if search_result==True:
-				return True
-			else:
-				return False
-		else:
-			print(str(data) + ' not found') 
-			return False
-
-	def delete(self,data):
-		if(self.parent):
-			deleted_element = self.parent.delete(data,parent=self.parent)
-			print(str(deleted_element) + ' was deleted')	
-		else:
-			print('Tree is empty')
-
-	def inorder(self):
-		if(self.parent):
-			self.parent.inorder()
-		else:
-			print('Tree is empty')
-
+bst = Tree(None)
 choice = 0
 while(choice != 5):
 	print("\n1) Press 1 to insert\n2) Press 2 to search\n3) Press 3 to Delete\n4) Press 4 to display inorder\n5) Press 5 to Quit")
@@ -123,6 +106,9 @@ while(choice != 5):
 		bst.find(data)
 	elif(choice==3):
 		data = int(input("\nEnter number to delete: "))
-		bst.delete(data)
+		if(bst.delete(data)):
+			print(str(data) + ' was deleted')
+		else:
+			print(str(data) + ' was not deleted')			
 	elif(choice==4):
-		bst.inorder()		
+		bst.inorder() 	
